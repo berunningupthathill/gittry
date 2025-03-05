1fbd716eb58c230d8f72b4ddbedb606a333a8b4a
berunningupthathill <151453979+berunningupthathill@users.noreply.github.com>
2025-03-05 23:59:50 +0800

    third commit

--- src/main/java/com/atguigu/Main.java
+++ src/main/java/com/atguigu/Main.java
@@ -2,6 +2,9 @@ package com.atguigu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hhh Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("master test");
+
     }
 }
